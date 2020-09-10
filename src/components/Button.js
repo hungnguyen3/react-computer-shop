8bd84bcef3c1e1f482cbd8d71421560fe172a833
hunglnl01 <hunglnl01@gmail.com>
2020-09-10 00:09:44 -0700

    working on user interface

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // style the button
 export const ButtonContainer = styled.button`
-    text-transform: capitalize;
-    font-size: 1.4rem;
-    background: transparent;
-    border: 0.08rem solid var(--lightBlue);
-    border-color: ${props =>props.cart? "var(--mainYellow)":"var(--lightBlue)"};
-    color: ${props=>props.cart?"var(--mainYellow)":"var(--lightBlue)"};
-    border-radius: 0.5rem;
-    padding: 0.2rem 0.5rem;
-    cursor: pointer;
-    margin: 0.2rem 0.5rem 0.2rem 0;
-    transition: all 0.5s ease-in-out;
-    &:hover{
-        background: ${props=>props.cart?"var(--mainYellow)":"var(--lightBlue)"};
-        color: var(--mainBlue);
-    }
-    &:focus{
-        outline: none;
-    }
-`
+	text-transform: capitalize;
+	font-size: 1.4rem;
+	background: transparent;
+	border: 0.08rem solid var(--navyBlue);
+	border-color: ${props =>
+		props.cart ? "var(--mainYellow)" : "var(--navyBlue)"};
+	color: ${props => (props.cart ? "var(--mainYellow)" : "var(--navyBlue)")};
+	border-radius: 0.5rem;
+	padding: 0.2rem 0.5rem;
+	cursor: pointer;
+	margin: 0.2rem 0.5rem 0.2rem 0;
+	transition: all 0.5s ease-in-out;
+	&:hover {
+		background: ${props =>
+			props.cart ? "var(--mainYellow)" : "var(--navyBlue)"};
+		color: var(--mainBlue);
+	}
+	&:focus {
+		outline: none;
+	}
+`;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,114 +1,124 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import {ProductConsumer} from "../context";
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
+import PropTypes from "prop-types";
 
 export default class Product extends Component {
-    render() {
-        const {id, title, img, price, inCart} = this.props.product;
-        return (
-            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-                {/* Card */}
-                <div className="card">
+	render() {
+		const { id, title, img, price, inCart } = this.props.product;
+		return (
+			<ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+				{/* Card */}
+				<div className="card">
+					<ProductConsumer>
+						{value => (
+							// image container
+							<div
+								className="img-container p-5"
+								onClick={() => {
+									value.handleDetail(id);
+								}}
+							>
+								{/* Link to access details */}
+								<Link to="/details">
+									{/* image here */}
+									<img src={img} alt="product" className="card-img-top"></img>
+								</Link>
+								{/* button to add to the cart */}
+								<button
+									className="cart-btn"
+									disabled={inCart ? true : false}
+									onClick={() => {
+										value.addToCart(id);
+										value.openModal(id);
+									}}
+								>
+									{inCart ? (
+										<p className="text-capitalize mb-0" disabled>
+											in cart
+										</p>
+									) : (
+										<i className="fas fa-cart-plus"></i>
+									)}
+								</button>
+							</div>
+						)}
+					</ProductConsumer>
 
-                    <ProductConsumer>
-                        {value => (
-                            // image container
-                            <div className="img-container p-5" onClick={()=>{value.handleDetail(id)}}>
-                                {/* Link to access details */}
-                                <Link to="/details">
-                                    {/* image here */}
-                                    <img src={img} alt="product" className="card-img-top"></img>
-                                </Link>
-                                {/* button to add to the cart */}
-                                <button className="cart-btn" 
-                                        disabled={inCart?true:false} 
-                                        onClick={()=>{
-                                            value.addToCart(id);
-                                            value.openModal(id);}}>
-                                        {inCart?(<p className="text-capitalize mb-0" disabled>in cart</p>):
-                                                (<i className="fas fa-cart-plus"></i>)}
-                                </button>
-                            </div>
-                        )}
-                    </ProductConsumer>
-
-                    {/* card footer */}
-                    <div className="card-footer d-flex justify-content-around">
-                        <p className="align-self-center mb-0">
-                            {title}
-                        </p>
-                        <h5 className="text-blue font-italic mb-0">
-                            <span className="mr-1">$</span>
-                            {price}
-                        </h5>
-                    </div>
-                </div>
-            </ProductWrapper>
-        )
-    }
+					{/* card footer */}
+					<div className="card-footer d-flex justify-content-around">
+						<p className="align-self-center mb-0">{title}</p>
+						<h5 className="text-blue font-italic mb-0">
+							<span className="mr-1">$</span>
+							{price}
+						</h5>
+					</div>
+				</div>
+			</ProductWrapper>
+		);
+	}
 }
 
 // setting up propTypes for the Product to catch input errors
 Product.propTypes = {
-    product:PropTypes.shape({
-        id:PropTypes.number,
-        img:PropTypes.string,
-        title:PropTypes.string,
-        price:PropTypes.number,
-        inCart:PropTypes.bool,
-    })
-}
+	product: PropTypes.shape({
+		id: PropTypes.number,
+		img: PropTypes.string,
+		title: PropTypes.string,
+		price: PropTypes.number,
+		inCart: PropTypes.bool,
+	}),
+};
 
 // setting styled component for ProductWrapper
 const ProductWrapper = styled.div`
-    .card{
-        border-color:transparent;
-        transition:all 1s linear;
-    }
-    .card-footer{
-        background:transparent;
-        border-top:transparent;
-        transition:all 1s linear;
-    }
-    &:hover{
-        .card{
-            border:0.08rem solid rgba(0,0,0,0.2);
-            box-shadow:5px 5px 8px 0px rgba(0,0,0,0.2);
-        }
-        .card-footer{
-            background:rgba(240, 240, 240);
-        }
-    }
-    .img-container{
-        position:relative;
-        overflow:hidden;
-    }
-    .card-img-top{
-        transition: all 1s linear;
-    }
-    .img-container:hover .card-img-top{
-        transform:scale(1.2)
-    }
-    .cart-btn{
-        position:absolute;
-        bottom:0;
-        right:0;
-        padding: 0.2rem 0.4rem;
-        background: var(--lightBlue);
-        border:none;
-        color: var(--mainWhite);
-        font-size:1.4rem;
-        border-radius: 0.5rem 0 0 0;
-        transform: translate(100%,100%);
-        transition: all 1s linear;
-    }
-    .img-container:hover .cart-btn{
-        transform: translate(0%,0%);
-    }
-    .cart-btn:hover{
-        color:var(--mainBlue);
-        cursor:pointer;
-    }
-`
+	.card {
+		border-color: transparent;
+		transition: all 1s linear;
+	}
+	.card-footer {
+		background: transparent;
+		border-top: transparent;
+		transition: all 1s linear;
+	}
+	&:hover {
+		.card {
+			border: 0.08rem solid rgba(0, 0, 0, 0.2);
+			box-shadow: 5px 5px 8px 0px rgba(0, 0, 0, 0.2);
+		}
+		.card-footer {
+			background: rgba(240, 240, 240);
+		}
+	}
+	.img-container {
+		position: relative;
+		overflow: hidden;
+	}
+	.card-img-top {
+		transition: all 1s linear;
+	}
+	.img-container:hover .card-img-top {
+		transform: scale(1.2);
+	}
+	.cart-btn {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		padding: 0.2rem 0.4rem;
+		background: var(--navyBlue);
+		border: none;
+		color: var(--mainWhite);
+		font-size: 1.4rem;
+		border-radius: 0.5rem 0 0 0;
+		transform: translate(100%, 100%);
+		transition: all 1s linear;
+	}
+	.img-container:hover .cart-btn {
+		transform: translate(0%, 0%);
+	}
+	.cart-btn:hover {
+		color: var(--mainBlue);
+		cursor: pointer;
+	}
+`;
